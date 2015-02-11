254092d7eda81ebbf086e5c714ef21554e829e6a
DouglasParanhos <douglasfaparanhos@gmail.com>
2015-02-11 18:29:52 -0200

    Create functions.php
    Arquivo com funções para formatar a saída do JSON e não ficar codificado errado.

---
+++ triplify1/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function utf8_strtr($str)
+{
+	$from = array( "á", "à", "â", "ã", "ä", "é", "è", "ê", "ë", "í", "ì", "î", "ï", "ó", "ò", "ô", "õ", "ö", "ú", "ù", "û", "ü", "ç" , "Á", "À", "Â", "Ã", "Ä", "É", "È", "Ê", "Ë", "Í", "Ì", "Î", "Ï", "Ó", "Ò", "Ô", "Õ", "Ö", "Ú", "Ù", "Û", "Ü", "Ç" ); 
+	$to = array( "a", "a", "a", "a", "a", "e", "e", "e", "e", "i", "i", "i", "i", "o", "o", "o", "o", "o", "u", "u", "u", "u", "c" , "A", "A", "A", "A", "A", "E", "E", "E", "E", "I", "I", "I", "I", "O", "O", "O", "O", "O", "U", "U", "U", "U", "C" ); 
+
+	return str_replace( $from, $to, $str); 
+}
+
+function format_property($property)
+{
+	$result = utf8_encode($property);
+	$result = utf8_strtr($result);
+	$result = str_replace(' ', '-', $result);
+	$result = strtolower($result);
+	
+	return  $result;
+}
+
+?>
