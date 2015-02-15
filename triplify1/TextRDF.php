597a03571a607d488a645dcad0eb377d5052b55c
DouglasParanhos <douglasfaparanhos@gmail.com>
2015-02-15 17:24:05 -0200

    Create TextRDF.php
    Classe que coloca as coisas em rdf

---
+++ triplify1/TextRDF.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once( "arc2/ARC2.php" );
+require_once( "functions.php" );
+
+class TextRDF {
+	
+	function __construct($posts) {
+
+		/*$parser = ARC2::getRDFParser();
+			$JSON = json_encode($posts);
+			$parser->parse($JSON);
+			print_r($parser);*/
+	
+		print_r($posts);
+	}
+	
+}
+
+?>
