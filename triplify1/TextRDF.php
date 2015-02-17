22729504ca5feadfb449960469841712ea900a08
DouglasParanhos <douglasfaparanhos@gmail.com>
2015-02-16 23:14:38 -0200

    Update TextRDF.php
    Ajustado para as tags ficarem perfeitas.

--- triplify1/TextRDF.php
+++ triplify1/TextRDF.php
@@ -6,8 +6,8 @@ require_once( "functions.php" );
 class TextRDF {
 	
 	function __construct($option_URI_base, $array_contendo_prefixos_usados, $posts) {
-		$RDF = '< '."?xml version=\"1.0\"?".' >';
-		$RDF = $RDF." < rdf:RDF ";
+		$RDF = '<?xml version="1.0"?>';
+		$RDF = $RDF."<rdf:RDF ";
 		$RDF = $RDF."xmlns:rdf= \"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" ";//every post have to have
 		
 		
@@ -17,24 +17,25 @@ class TextRDF {
 			//else if(strcmp(strtolower($object->prefix), 'rdf') == 0) $RDF = $RDF.$object->prefix."= \"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" ";
 			else if(strcmp(strtolower($object->prefix), 'rdfs') == 0) $RDF = $RDF."xmlns:".$object->prefix."= \"http://www.w3.org/2000/01/rdf-schema#\" ";
 		}
-		$RDF = $RDF." >";
+		$RDF = $RDF.">";
 		
 		foreach($posts as $post){
-			$RDF= $RDF."< rdf:Description ";
+			$RDF= $RDF."<rdf:Description ";
 			$RDF= $RDF."rdf:about=\"".$option_URI_base.$post->ID."\"";
-			$RDF= $RDF." > ";
+			$RDF= $RDF.">";
 			foreach($array_contendo_prefixos_usados as $object){
 				$property = $object->fullProperty;
-				$RDF = $RDF."< ".$object->fullProperty." > ";
+				$RDF = $RDF."<".$object->fullProperty.">";
 				$RDF = $RDF.$post->$property;
-				$RDF = $RDF." < /".$object->fullProperty." > ";
+				$RDF = $RDF."</".$object->fullProperty.">";
 			}
-			$RDF= $RDF." < /rdf:Description > ";
+			$RDF= $RDF."</rdf:Description>";
 		}
 		
-		$RDF = $RDF." < /rdf:RDF > ";
+		$RDF = $RDF." </rdf:RDF>";
 	
-		echo $RDF;
+		echo htmlentities($RDF);
+		//echo $RDF;
 	}
 	
 }
