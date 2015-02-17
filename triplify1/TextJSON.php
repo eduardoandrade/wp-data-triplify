307310a34562188f77d75850145b2f0d752f7875
DouglasParanhos <douglasfaparanhos@gmail.com>
2015-02-17 20:13:16 -0200

    Update TextJSON.php
    JSON-LD funcionando perfeitamente.

--- triplify1/TextJSON.php
+++ triplify1/TextJSON.php
@@ -16,40 +16,40 @@ class TextJSON {
 			foreach($array_contendo_prefixos_usados as $object){
 				if(strcmp(getPrefix($object->prefix), 'dc') == 0){
 					if($object->uri == 1){
-						$context = array($object->prefix => (object)array("@id" => "http://purl.org/dc/elements/1.1".$object->prefix, "@type" => "@id"));//ATENÇÃÃÃÃÃÃÃO
+						$context = array_merge($context, array($object->prefix => (object)array("@id" => "http://purl.org/dc/elements/1.1".$object->prefix, "@type" => "@id")));//ATENÇÃÃÃÃÃÃÃO
 					} else {
-						$context = array($object->prefix => (object)array("@id" => "http://purl.org/dc/elements/1.1".$object->prefix));//ATENÇÃÃÃÃÃÃÃO
+						$context = array_merge($context, array($object->prefix => (object)array("@id" => "http://purl.org/dc/elements/1.1".$object->prefix)));//ATENÇÃÃÃÃÃÃÃO
 					}
 				} else if(strcmp(getPrefix($object->prefix),'foaf') == 0){
 					if($object->uri == 1){
-						$context = array($object->prefix => (object)array("@id" => "http://xmlns.com/foaf/0.1/".$object->prefix, "@type" => "@id"));//ATENÇÃÃÃÃÃÃÃO
+						$context = array_merge($context, array($object->prefix => (object)array("@id" => "http://xmlns.com/foaf/0.1/".$object->prefix, "@type" => "@id")));//ATENÇÃÃÃÃÃÃÃO
 					} else {
-						$context = array($object->prefix => (object)array("@id" => "http://xmlns.com/foaf/0.1/".$object->prefix));//ATENÇÃÃÃÃÃÃÃO
+						$context = array_merge($context, array($object->prefix => (object)array("@id" => "http://xmlns.com/foaf/0.1/".$object->prefix)));//ATENÇÃÃÃÃÃÃÃO
 					}
 				} else if(strcmp(getPrefix($object->prefix), 'owl') == 0){
 					if($object->uri == 1){
-						$context = array($object->prefix => (object)array("@id" => "http://www.w3.org/2002/07/owl#".$object->prefix, "@type" => "@id"));//ATENÇÃÃÃÃÃÃÃO
+						$context = array_merge($context, array($object->prefix => (object)array("@id" => "http://www.w3.org/2002/07/owl#".$object->prefix, "@type" => "@id")));//ATENÇÃÃÃÃÃÃÃO
 					} else {
-						$context = array($object->prefix => (object)array("@id" => "http://www.w3.org/2002/07/owl#".$object->prefix));//ATENÇÃÃÃÃÃÃÃO
+						$context = array_merge($context, array($object->prefix => (object)array("@id" => "http://www.w3.org/2002/07/owl#".$object->prefix)));//ATENÇÃÃÃÃÃÃÃO
 					}
 				}
 				else if(strcmp(getPrefix($object->prefix), 'rdf') == 0){
 					if($object->uri == 1){
-						$context = array($object->prefix => (object)array("@id" => "http://www.w3.org/1999/02/22-rdf-syntax-ns#".$object->prefix, "@type" => "@id"));//ATENÇÃÃÃÃÃÃÃO
+						$context = array_merge($context, array($object->prefix => (object)array("@id" => "http://www.w3.org/1999/02/22-rdf-syntax-ns#".$object->prefix, "@type" => "@id")));//ATENÇÃÃÃÃÃÃÃO
 					} else {
-						$context = array($object->prefix => (object)array("@id" => "http://www.w3.org/1999/02/22-rdf-syntax-ns#".$object->prefix));//ATENÇÃÃÃÃÃÃÃO
+						$context = array_merge($context, array($object->prefix => (object)array("@id" => "http://www.w3.org/1999/02/22-rdf-syntax-ns#".$object->prefix)));//ATENÇÃÃÃÃÃÃÃO
 					}
 				} else if(strcmp(getPrefix($object->prefix), 'rdfs') == 0){
 					if($object->uri == 1){
-						$context = array($object->prefix => (object)array("@id" => "http://www.w3.org/2000/01/rdf-schema#".$object->prefix, "@type" => "@id"));//ATENÇÃÃÃÃÃÃÃO
+						$context = array_merge($context, array($object->prefix => (object)array("@id" => "http://www.w3.org/2000/01/rdf-schema#".$object->prefix, "@type" => "@id")));//ATENÇÃÃÃÃÃÃÃO
 					} else {
-						$context = array($object->prefix => (object)array("@id" => "http://www.w3.org/2000/01/rdf-schema#".$object->prefix));//ATENÇÃÃÃÃÃÃÃO
+						$context = array_merge($context, array($object->prefix => (object)array("@id" => "http://www.w3.org/2000/01/rdf-schema#".$object->prefix)));//ATENÇÃÃÃÃÃÃÃO
 					}
 				} else if(strcmp(getPrefix($object->prefix), 'xsd') == 0){
 					if($object->uri == 1){
-						$context = array($object->prefix => (object)array("@id" => "http://www.w3.org/2001/XMLSchema#".$object->prefix, "@type" => "@id"));//ATENÇÃÃÃÃÃÃÃO
+						$context = array_merge($context, array($object->prefix => (object)array("@id" => "http://www.w3.org/2001/XMLSchema#".$object->prefix, "@type" => "@id")));//ATENÇÃÃÃÃÃÃÃO
 					} else {
-						$context = array($object->prefix => (object)array("@id" => "http://www.w3.org/2001/XMLSchema#".$object->prefix));//ATENÇÃÃÃÃÃÃÃO
+						$context = array_merge($context, array($object->prefix => (object)array("@id" => "http://www.w3.org/2001/XMLSchema#".$object->prefix)));//ATENÇÃÃÃÃÃÃÃO
 					}
 				}
 			}
