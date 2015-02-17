fd4db84bd1a91763e19249b46dbd0bb2bdbd3ef5
DouglasParanhos <douglasfaparanhos@gmail.com>
2015-02-17 13:10:45 -0200

    Update TextXML.php
    XML perfeito

--- triplify1/TextXML.php
+++ triplify1/TextXML.php
@@ -5,16 +5,16 @@ require_once( "functions.php" );
 
 class TextXML {
 	
-	function __construct($option_URI_base, $array_contendo_prefixos_usados, $posts) {
+	function __construct($option_URI_base, $array_contendo_objetos_usados, $prefixos, $posts) {
 		echo htmlentities('<?xml version="1.0"?>');
 		echo htmlentities ("<posts>");
 		foreach($posts as $post){
 			$XML = "<post ";
-			foreach($array_contendo_prefixos_usados as $object){//always there will be at maximum one of each.
-				if(strcmp(strtolower($object->prefix), 'dc') == 0) $XML = $XML."xmlns:".$object->prefix."= \"http://purl.org/dc/elements/1.1\" ";
-				else if(strcmp(strtolower($object->prefix), 'foaf') == 0) $XML = $XML."xmlns:".$object->prefix."= \"http://xmlns.com/foaf/0.1/\" ";
-				else if(strcmp(strtolower($object->prefix), 'rdf') == 0) $XML = $XML."xmlns:".$object->prefix."= \"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" ";
-				else if(strcmp(strtolower($object->prefix), 'rdfs') == 0) $XML = $XML."xmlns:".$object->prefix."= \"http://www.w3.org/2000/01/rdf-schema#\" ";
+			foreach($prefixos as $prefixo){//always there will be at maximum one of each.
+				if(strcmp(strtolower($prefixo), 'dc') == 0) $XML = $XML."xmlns:".$prefixo."= \"http://purl.org/dc/elements/1.1\" ";
+				else if(strcmp(strtolower($prefixo), 'foaf') == 0) $XML = $XML."xmlns:".$prefixo."= \"http://xmlns.com/foaf/0.1/\" ";
+				else if(strcmp(strtolower($prefixo), 'rdf') == 0) $XML = $XML."xmlns:".$prefixo."= \"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" ";
+				else if(strcmp(strtolower($prefixo), 'rdfs') == 0) $XML = $XML."xmlns:".$prefixo."= \"http://www.w3.org/2000/01/rdf-schema#\" ";
 			}
 			$XML = $XML.">";
 			$XML = $XML."<URI>";
@@ -22,7 +22,7 @@ class TextXML {
 			$XML = $XML."</URI>";
 			echo htmlentities($XML);
 			
-			foreach($array_contendo_prefixos_usados as $object){
+			foreach($array_contendo_objetos_usados as $object){
 				$property = $object->fullProperty;
 				echo htmlentities("<".$object->fullProperty.">");
 				echo $post->$property;
