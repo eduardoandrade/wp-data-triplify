45650197096ea140a7e73c89041c0b9bf5ee720d
DouglasParanhos <douglasfaparanhos@gmail.com>
2015-02-16 23:15:15 -0200

    Update TextXML.php
    ajustado para as tags ficarem perfeitas.

--- triplify1/TextXML.php
+++ triplify1/TextXML.php
@@ -6,22 +6,22 @@ require_once( "functions.php" );
 class TextXML {
 	
 	function __construct($option_URI_base, $array_contendo_prefixos_usados, $posts) {
-		 $XML = '< '."?xml version=\"1.0\"?".' >';
+		 $XML = '<?xml version="1.0?>';
 		foreach($posts as $post){
-			$XML = $XML."< POST>";
-			$XML = $XML."< URI>";
+			$XML = $XML."<POST>";
+			$XML = $XML."<URI>";
 			$XML = $XML.$option_URI_base.$post->ID;
-			$XML = $XML."< /URI>";
+			$XML = $XML."</URI>";
 			foreach($array_contendo_prefixos_usados as $object){
 				$property = $object->fullProperty;
-				 $XML =  $XML."< ".$object->fullProperty." > ";
+				 $XML =  $XML."<".$object->fullProperty.">";
 				 $XML =  $XML.$post->$property;
-				 $XML =  $XML." < /".$object->fullProperty." > ";
+				 $XML =  $XML."</".$object->fullProperty.">";
 			}
-			$XML = $XML."< /POST>";
+			$XML = $XML."</POST>";
 		}
 		
-		echo $XML;
+		echo htmlentities($XML);
 	}
 	
 }
