392413974f3f64f81f3b2a1831d04e617d454e16
DouglasParanhos <douglasfaparanhos@gmail.com>
2015-02-19 14:55:44 -0200

    Update render.php
    Adicionada parte de fazer upload de arquivo com configurações mas ainda não funciona

--- triplify1/render.php
+++ triplify1/render.php
@@ -1,4 +1,7 @@
 <?php
+
+include_once('ReadCSVFile.php');
+
 class Render {
 	
 	function __construct() {
@@ -8,22 +11,47 @@ class Render {
 	do_action('admin_enqueue_scripts');
 	do_action('wp_ajax_my_action');*/
 		
-		if(!isset($_POST['termoPesquisado'])){
+		if(!isset($_POST['termoPesquisado']) && !isset($_POST['triplify-csv-file'])){
 	?>
 			<div>
-			<form action="" method="POST">
-				<h3>Digite a URL que deseja acessar para verificar os dados triplificados:</h3>
-				<br/>
-					<code><?php bloginfo('url'); ?>/</code> <input name="url_base" value="<?php echo get_option("triplify_url_base_dados", "tri");?>" id="postType_base"/>
-				<h3>Digite o post-type que deseja triplificar: </h3>
-				<br/>
+				<form action="" method="POST">
+					<h3>Digite a URL que deseja acessar para verificar os dados triplificados:</h3>
+					<br/>
+						<code><?php bloginfo('url'); ?>/</code> <input name="url_base" value="<?php echo get_option("triplify_url_base_dados", "tri");?>" id="postType_base"/>
+					<h3>Post-types já triplificados:<h3>
+					<br/>
+						<?php 
+							global $wpdb;
+							$url = $url = get_bloginfo('url');
+							$url_base = get_option("triplify_url_base_dados", "tri");
+							$posts = $wpdb->get_results("SELECT distinct tipo FROM {$wpdb->prefix}triplify_configurations");
+
+							if(!empty($posts)){
+								foreach($posts as $tipo){
+									echo "<code><a href=\"$url\\$url_base\\$tipo->tipo\">$tipo->tipo</a></code>" ;
+								}
+							} else {
+								echo "Nenhum tipo triplificado ainda.";
+							}
+						?>
+					<h3>Desejo pesquisar o post-type e configurá-lo manualmente para triplificar: </h3>
+					<br/>
 					<input name="postType" value="" id="postType"/>
 					<button name="termoPesquisado" type="submit" class="button-primary">Pesquisar</button>
-				</form>	
-				<br/>
-			</div>
+				</form>
+				<form action="" method="POST">
+					<h3>Desejo fazer leitura de arquivo CSV com tipo e configurações lá contidas: </h3>
+					<br/>
+					<div class="pure-control-group">
+						<label>Arquivo</label>
+						<input id="triplify-file" name="triplify-csv-file" type="file" value="" data-validate="validate(required)" />
+						<button name="upload_arquivo" type="submit" class="button-primary">Importar</button>
+					</div>
+				</div>
+			</form>
+		</div>
 	<?php
-		} else {
+		} else if(isset($_POST["postType"])){
 			$termo = $this->pegaValores($_POST["postType"]);
 			$this->salvaUrlBase($_POST["url_base"]);
 	?>
@@ -38,7 +66,7 @@ class Render {
 				
 				<h4>Defina as equivalências e marque o checkbox caso o resultado mostrado por essa coluna seja uma URI: </h4>
 				<h6>Marque a opção "não me interessa" caso o valor daquela correspondência não te importar.</h6>
-				<h6>Caso essa opção seja marcada a coluna não aparecerá nos dados triplificados</h6>
+				<h6>Caso essa opção seja marcada a coluna não aparecerá nos dados triplificados.</h6>
 				<h6>Selecionar o "não me interessa" ou deixar a correspondência com vazio ou "correspondencia" dá no mesmo.</h6>
 				<h6>O checkbox serve apenas para a página ficar mais limpa caso o usuário ache que esteja muito poluída.</h6>
 <?php
@@ -100,16 +128,21 @@ class Render {
 				<br/>
 				<button id="id" name="triplify" class="button-primary">Salvar opções</button>
 			</div><?php
+		} else if(isset($_POST['triplify-csv-file'])){
+			
+			if((new ReadCSVFile($_POST['triplify-csv-file'])) == true) echo "SALVOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!";
+			else echo "OVLASSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS";
+			
 		}?>
 		<div id="corpo2" style="display:none">
 			<h2>Opções salvas!</h2>
-			<h3>Acesse <code><?php bloginfo('url');?><?php echo get_option("triplify_url_base_dados", "tri")?>/<?php echo $termo; ?>/formato_desejado_dos_dados</code> para obter os dados. Caso o formato não seja especificado, o resultado será mostrado em JSON</h3>
+			<h3>Acesse <code><?php bloginfo('url');?>/<?php echo get_option("triplify_url_base_dados", "tri")?>/<?php echo $termo; ?>/formato_desejado_dos_dados</code> para obter os dados. Caso o formato não seja especificado, o resultado será mostrado em JSON</h3>
 		</div>
 		<?php
 	
 		function triplify_javascript() { ?>
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
+			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+			<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
 			<script type="text/javascript">
 			jQuery(document).ready(function($) {
 				$("#id").click(function(){
