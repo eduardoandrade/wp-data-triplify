ea732b7bfc1269dda2b1c7ba0defe76ddf4958c0
DouglasParanhos <douglasfaparanhos@gmail.com>
2015-05-09 22:59:52 -0300

    Update TextRDF.php

--- TextRDF.php
+++ TextRDF.php
@@ -3,28 +3,29 @@
 include_once( "arc2/ARC2.php" );
 require_once( "functions.php" );
 
-class dt_TextRDF {
+class dt_TextXML {
 	
 	function __construct($option_URI_base, $array_contendo_objetos_usados, $prefixos, $posts) {
-		
 		global $wpdb;
 		$prefixos_banco = $wpdb->get_results("SELECT * FROM wp_triplify_prefixes");
 		
-		$RDF = '<?xml version="1.0" encoding="UTF-8"?>';
-		$RDF = $RDF."<rdf:RDF ";
-		$RDF = $RDF."xmlns:rdf= \"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" ";//every post have to have
-		
-		foreach($prefixos_banco as $prefix){//always there will be at maximum one of each.
-			if(in_array(strtolower($prefix->prefixo), $prefixos) && $prefix->prefixo != "rdf") $RDF = $RDF."xmlns:".$prefix->prefixo."= "."\"$prefix->uri\" ";
-		}
-		$RDF = $RDF.">";
-		echo htmlentities($RDF);
-		
+		echo htmlentities('<?xml version="1.0"?>');
+		echo htmlentities ("<posts>");
 		foreach($posts as $post){
-			$RDF= "<rdf:Description ";
-			$RDF= $RDF."rdf:about=\"".$option_URI_base.$post->ID."\"";
-			$RDF= $RDF.">";
-			echo htmlentities($RDF);
+			$XML = "<post ";
+			foreach($prefixos_banco as $prefix){//always there will be at maximum one of each.
+				if(in_array(strtolower($prefix->prefixo), $prefixos)){
+					if(substr($prefix->uri, -1) != "/"){
+						$prefix->uri = $prefix->uri."/";
+					}
+					$XML = $XML."xmlns:".$prefix->prefixo."= "."\"$prefix->uri\" ";
+				}
+			}
+			$XML = $XML.">";
+			$XML = $XML."<URI>";
+			$XML = $XML.$option_URI_base.$post->ID;
+			$XML = $XML."</URI>";
+			echo htmlentities($XML);
 			
 			foreach($array_contendo_objetos_usados as $object){
 				$property = $object->fullProperty;
@@ -32,10 +33,9 @@ class dt_TextRDF {
 				echo $post->$property;
 				echo htmlentities("</".$object->fullProperty.">");
 			}
-			echo htmlentities("</rdf:Description>");
+			echo htmlentities("</post>");
 		}
-		
-		echo htmlentities("</rdf:RDF>");
+		echo htmlentities ("</posts>");
 	}
 	
 }
